9bdce30e3bc014bab381df05189dcb49b6a3a1e9
Carlos Agustin Diaz <carlos.agustin.diaz1998@gmail.com>
2020-02-09 20:53:47 -0300

    webmix v2

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,15 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .js('node_modules/swiper/js/swiper.min.js','public/js/swiper.js')
+   .sass('resources/sass/app.scss', 'public/css')
+   .styles('node_modules/bootstrap/dist/css/bootstrap.min.css','public/css/bootstrap.css')
+   .styles('node_modules/mdbootstrap/css/mdb.min.css','public/css/mdb.css')
+   .styles('node_modules/swiper/css/swiper.min.css','public/css/swiper.css');
+
+mix.combine([
+      'node_modules/jquery/dist/jquery.js',
+      'node_modules/bootstrap/dist/js/bootstrap.min.js',
+      'node_modules/swiper/js/swiper.min.js',
+   ],'public/js/jquery.js');
